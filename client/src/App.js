24fd19ae45208a0d5ff9652fe2ac5e78e0b6e649
Damish-N <damishnisal100@gmial.com>
2021-05-14 02:11:49 +0530

    made the InterFace

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import "./App.css";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Student from "./component/showStudent/showStudent";
+import CreateStudent from "./component/createStudent/createStudent";
+import useStyles from "./style.js";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div>
       <Container maxWidth="lg">
-        <AppBar>hh</AppBar>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            STUDENT SHOW AND CREATE
+          </Typography>
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              <Grid item xs={12} sm={7}>
+                <AppBar
+                  className={classes.appBar}
+                  position="static"
+                  color="inherit"
+                >
+                  <Student></Student>
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar
+                  className={classes.appBar}
+                  position="static"
+                  color="inherit"
+                >
+                  <CreateStudent></CreateStudent>
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
       </Container>
     </div>
   );
---
+++ client/src/style.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: "25px",
+    margin: "30px 0px",
+    display: "flex",
+    flexDirection: "coloumn",
+    justifyContent: "center",
+    alignItem: "center",
+  },
+}));
