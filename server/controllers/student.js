02a317215fab6cb854da9e6b228de9c9fa5b846b
Damish-N <damishnisal100@gmial.com>
2021-05-13 22:45:55 +0530

    create model of the student

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,5 +1,22 @@
-const getStudent = function (req, res) {
-  res.send("roter is working");
+import student from "../models/student.js";
+
+export const getStudents = async function (req, res) {
+  try {
+    const allStudents = await student.find();
+    res.status(200).json(allStudents);
+  } catch (error) {
+    res.status(404).json({ massage: error });
+  }
 };
 
-export default getStudent;
+export const createStudent = async function (req, res) {
+  const cstudent = req.body;
+  const newStudent = new student(cstudent);
+
+  try {
+    await newStudent.save();
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(409).json({ massage: error });
+  }
+};
